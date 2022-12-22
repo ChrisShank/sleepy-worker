fc9e713167986d37c0ffd8e238ee1fbaf3e904d8
“chrisshank” <chris.shank.23@gmail.com>
2022-12-22 01:34:28 -0500

    comment code, add confirm dialog

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-// Index 0 represents the monotonic count to notify the worker of a result
-// Index 1 represents the result of the condition
+// Reference on Atomics: https://github.com/tc39/proposal-ecmascript-sharedmem/blob/main/TUTORIAL.md
+
+// Allocate a shared buffer with a single byte.
+// -1 represents the value being unset
+// 0 represents a falsy value
+// 1 represents a truthy value
 const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT);
 const sharedArray = new Int32Array(sharedBuffer);
+sharedArray[0] = -1;
+
 const worker = new Worker(new URL('./worker', import.meta.url), { type: 'module' });
 
 worker.addEventListener('message', (e) => {
   if (e.data.type === 'EVALUATE_CONDITION') {
-    setTimeout(() => {
-      Atomics.store(sharedArray, 0, 1);
-      console.log('notify');
-    }, 1000);
+    const response = confirm(`Is the condition '${e.data.conditionName} true?`);
+    notifyWorker(response);
   }
 });
 
+function notifyWorker(response: boolean) {
+  // update shared array with response
+  Atomics.store(sharedArray, 0, response ? 1 : 0);
+  // notify worker that there is a response
+  Atomics.notify(sharedArray, 0, 1);
+}
+
+// send shared array to worker
 worker.postMessage({ type: 'INITIALIZE_SHARED_BUFFER', sharedBuffer });
 
 export {};
--- src/worker.ts
+++ src/worker.ts
@@ -3,12 +3,27 @@ let sharedArray: Int32Array;
 self.addEventListener('message', (e) => {
   if (e.data.type === 'INITIALIZE_SHARED_BUFFER') {
     sharedArray = new Int32Array(e.data.sharedBuffer);
-
-    Atomics.store(sharedArray, 0, -1); // reset buffer
-    self.postMessage({ type: 'EVALUATE_CONDITION', conditionName: 'isTrue' }); // notify main thread to evaluate condition
-    while (Atomics.load(sharedArray, 0) == -1) {} // block until memory is changed
-    console.log(Atomics.load(sharedArray, 0));
+    evaluateCondition('isTrue');
+    // adding a timeout to prompt again confirms that the worker is put to sleep.
+    setTimeout(() => {
+      evaluateCondition('isFalse');
+    }, 1000);
   }
 });
+
+function evaluateCondition(conditionName: string) {
+  // notify main thread to evaluate condition
+  self.postMessage({ type: 'EVALUATE_CONDITION', conditionName });
+
+  // reset value in buffer
+  Atomics.store(sharedArray, 0, -1);
+
+  // wait for main thread to respond by putting the worker to sleep
+  // The worker will only sleep if the last argument equal to the *current* value at the specified index.
+  // Sleeping is preferred to synchronously blocking which is more wasteful
+  Atomics.wait(sharedArray, 0, -1);
+  const response = Atomics.load(sharedArray, 0) === 1;
+  console.log(response);
+}
 
 export {};
